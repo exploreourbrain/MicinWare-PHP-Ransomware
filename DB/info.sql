1a699c9e5cbb5283d82e26661bfe7eac9bec8769
Roby Firnando Yusuf <nandoapuesto19@gmail.com>
2019-02-20 17:55:55 +0700

    0ops

--- DB/info.sql
+++ DB/info.sql
@@ -15,7 +15,4 @@ CREATE TABLE `info` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO `info` (`id`, `host`, `path_infected`, `key`, `attacked_at`) VALUES
-(1,	'http://localheart/ransom/ransom1.php',	'/var/www/html/ransom/kelinci/',	'3361e456a4eaf790b94bdae2e22c10d7',	'2019-02-20 17:04:45');
-
 -- 2019-02-20 10:49:12
